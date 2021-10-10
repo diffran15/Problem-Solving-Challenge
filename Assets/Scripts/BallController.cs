7b979d2021514688db806edc2d5d4727a13c6078
diffran15 <diffran5@gmail.com>
2021-10-11 02:17:25 +0700

    problem-4 fixed

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField]
+    public float Speed;
+
+    private Rigidbody2D rb;
+    public float yBoundary = 6.5f;
+    public float xBoundary = 2.5f;
+
+    public KeyCode upButton = KeyCode.W;
+    public KeyCode leftButton = KeyCode.A;
+    public KeyCode downButton = KeyCode.S;
+    public KeyCode rightButton = KeyCode.D;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Update()
+    {
+        Vector2 velocity = rb.velocity;
+
+        if (Input.GetKey(upButton))
+        {
+            velocity.y = Speed;
+        }
+        else if (Input.GetKey(downButton))
+        {
+            velocity.y = -Speed;
+        }
+        else
+        {
+            velocity.y = 0.0f;
+        }
+
+        if (Input.GetKey(rightButton))
+        {
+            velocity.x = Speed;
+        }
+        else if (Input.GetKey(leftButton))
+        {
+            velocity.x = -Speed;
+        }
+        else
+        {
+            velocity.x = 0.0f;
+        }
+
+        rb.velocity = velocity;
+    }
+}
