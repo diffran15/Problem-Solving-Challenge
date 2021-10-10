1e5b7978c0e8b104eb2e1832985e3509366f69d1
diffran15 <diffran5@gmail.com>
2021-10-11 01:43:42 +0700

    problem-2

---
+++ Assets/Scripts/BallMoveController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMoveController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float speedBall;
+
+    private Rigidbody2D rb;
+    private Vector2 velocity;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        velocity = new Vector2(speedBall, 0);
+
+    }
+
+    private void FixedUpdate()
+    {
+
+
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+
+    }
+}
