cf263486eedc4c553309959fa4f83227fe8f1e5b
diffran15 <diffran5@gmail.com>
2021-10-11 02:00:48 +0700

    problem-3

---
+++ Assets/Scripts/BallMoveController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMoveController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float xForce;
+    public float yForce;
+
+    private Rigidbody2D rb;
+    
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        PushBall();
+
+    }
+    void PushBall()
+    {
+        float randomDirection = Random.Range(0, 2);
+        if (randomDirection < 1.0f)
+        {
+            rb.AddForce(new Vector2(xForce, yForce));
+        }
+        else
+        {
+            rb.AddForce(new Vector2(-xForce, yForce));
+        }
+    }
+    
+}
