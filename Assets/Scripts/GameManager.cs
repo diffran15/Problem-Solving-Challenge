d4d3185f8350b56c119e496c04afe1e78d18c08c
diffran15 <diffran5@gmail.com>
2021-10-11 02:47:51 +0700

    problem-7

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance = null;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+            }
+            return _instance;
+        }
+    }
+
+
+    public Text ScoreNumber;
+    private double _totalScore;
+
+    public void ScoreIncrement(double value)
+    {
+        _totalScore += value;
+        ScoreNumber.text = $"{_totalScore.ToString("0")}";
+    }
+}
